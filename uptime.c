2c6b82aba8d6311a8f3a5f94aa76e7f99b59ed8e
Jorgemayor <jorge.e.mayor@gmail.com>
2020-05-30 04:23:53 +0000

    Working with fixed ticks

--- uptime.c
+++ uptime.c
@@ -4,8 +4,36 @@
 #include "fcntl.h"
 
 int
-main(int argc, char *agrv[])
+main(int argc, char *argv[])
 {
-	printf(2, "uptime working\n");
+
+	int ticks = 100;
+	if(argc > 1)
+	{
+		//ticks = atoi(argv[1]);
+	}
+
+	int time = uptime();
+	printf(2, "%d\n", time);
+	if(time < 60)
+	{
+		printf(2, "Time: %d\n", time);
+	}
+	else
+	{
+		int seconds = time%60;
+		int minutes = time/60;
+		if(minutes < 60)
+		{
+			printf(2, "Time: %d:%d\n", minutes, seconds);
+		}
+		else
+		{
+			int hours = minutes/60;
+			minutes = minutes%60;
+			printf(2, "Time: %d:%d:%d\n", hours, minutes, seconds);
+		} 
+	}
+	printf(2, "uptime working %d %d\n", ticks, time);
 	exit();
 }
