ec35a791b663ef1ecb52f42d0ae2300b10beae32
Jorgemayor <jorge.e.mayor@gmail.com>
2020-05-30 01:01:32 +0000

    Added prototype functions

--- defs.h
+++ defs.h
@@ -186,5 +186,10 @@ void            switchkvm(void);
 int             copyout(pde_t*, uint, void*, uint);
 void            clearpteu(pde_t *pgdir, char *uva);
 
+// extra
+int		uptime(void);		
+int		date(void);
+int		numinv(void);
+
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
