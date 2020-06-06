037c5be37b7d6adaa913cd7016cd292676e6c5f2
Jorgemayor <jorge.e.mayor@gmail.com>
2020-06-06 04:34:05 +0000

    Fixed ticks and output

--- uptime.c
+++ uptime.c
@@ -7,17 +7,12 @@ int
 main(int argc, char *argv[])
 {
 
-	int ticks = 100;
-	if(argc > 1)
-	{
-		//ticks = atoi(argv[1]);
-	}
-
+	int ticks = 4000;
 	int time = 60*uptime()/ticks;
 
 	if(time < 60)
 	{
-		printf(2, "Time: %d\n", time);
+		printf(2, "Time: %d Ticks: %d\n", time, ticks);
 	}
 	else
 	{
@@ -25,13 +20,13 @@ main(int argc, char *argv[])
 		int minutes = time/60;
 		if(minutes < 60)
 		{
-			printf(2, "Time: %d:%d\n", minutes, seconds);
+			printf(2, "Time: %d:%d Ticks: %d\n", minutes, seconds, ticks);
 		}
 		else
 		{
 			int hours = minutes/60;
 			minutes = minutes%60;
-			printf(2, "Time: %d:%d:%d\n", hours, minutes, seconds);
+			printf(2, "Time: %d:%d:%d Ticks: %d\n", hours, minutes, seconds, ticks);
 		} 
 	}
 	exit();
